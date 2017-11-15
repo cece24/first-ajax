6621f1df72a3127b2d55c25a5aced72774b04804
Cecilia Wong <ceciliakhwong@gmail.com>
2017-11-15 16:43:04 -0500

    Add button for step 2, makes AJAX request upon click

--- ajax.js
+++ ajax.js
@@ -1,10 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-
+  var step2Button = document.querySelector('#step12 > button');
   /* Your code goes here */
-  $.ajax({
-    url: 'http://first-ajax-api.herokuapp.com/',
-    method: 'GET',
-    dataType: 'text'
+  step2Button.addEventListener('click', function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/',
+      method: 'GET',
+      dataType: 'text'
+    });
   });
-
+  
 });
